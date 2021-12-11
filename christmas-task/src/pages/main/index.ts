08b064203d49a6e5a6055ab38c9f0d9d95a7f013
Taneros <fricentaurus@GMAIL.COM>
2021-12-11 13:15:52 +0300

    refactor: remove append h1 tag in main page ts & reuse component

--- christmas-task/src/pages/main/index.ts
+++ christmas-task/src/pages/main/index.ts
@@ -1,30 +1,22 @@
-import Component from '../../core/templates/components';
+import Main from '../../core/components/main';
 import Page from '../../core/templates/page';
-
-class Main extends Component {
-  constructor(tagName: string, className: string) {
-    super(tagName, className);
-  }
-  render() {
-    return this.container;
-  }
-}
-
-const mainHTML = new Main('main', 'main');
-const mainHTMLrenderred: HTMLElement = mainHTML.render();
 
 class MainPage extends Page {
   static TextObject = {
     MainTitle: 'Main Page',
   };
 
+  mainHTML: Main;
+
   constructor(id: string, className: string) {
     super(id, className);
+    this.mainHTML = new Main('main', 'main');
   }
 
   render() {
-    const title = this.createHeaderTitle(MainPage.TextObject.MainTitle);
-    this.container.append(title);
+    // const title = this.createHeaderTitle(MainPage.TextObject.MainTitle);
+    // this.container.append(title);
+    const mainHTMLrenderred: HTMLElement = this.mainHTML.render();
     mainHTMLrenderred.append(this.container);
     return mainHTMLrenderred;
   }
