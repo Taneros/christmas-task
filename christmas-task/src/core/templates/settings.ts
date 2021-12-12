761f37064ba191fcb9da3ef667d733bab847403d
Taneros <fricentaurus@GMAIL.COM>
2021-12-12 14:40:12 +0300

    fix: change data filter attr

--- christmas-task/src/core/templates/settings.ts
+++ christmas-task/src/core/templates/settings.ts
@@ -2,13 +2,21 @@ class SettingsSections {
   static controls = `
       <div class="controls__filter-by-value">
         <h2 class="controls__filter-header">Sort by Value</h2>
-        <div class="filter__btns">
-          <h3 class="filter-btns__header">Form</h3>
+        <div id="filter-by-shape" class="filter__btns">
+          <h3 class="filter-btns__header">Shape</h3>
           <button class="filter__btns btn" data-filter="шар"></button>
           <button class="filter__btns btn" data-filter="колокольчик"></button>
           <button class="filter__btns btn" data-filter="шишка"></button>
           <button class="filter__btns btn" data-filter="снежинка"></button>
           <button class="filter__btns btn" data-filter="фигурка"></button>
+      </div>
+      <div id="filter-by-color" class="filter__btns">
+          <h3 class="filter-btns__header">Color</h3>
+          <button class="filter__btns btn" data-filter="белый"></button>
+          <button class="filter__btns btn" data-filter="желтый"></button>
+          <button class="filter__btns btn" data-filter="красный"></button>
+          <button class="filter__btns btn" data-filter="синий"></button>
+          <button class="filter__btns btn" data-filter="зелёный"></button>
       </div>
       </div>
         <div class="controls__filter-by-range">
