12f7a32cb00e927607f16f9367abd119f208ff28
Taneros <fricentaurus@GMAIL.COM>
2021-12-15 18:17:28 +0300

    feat: add sort by value styles

--- christmas-task/src/core/templates/settings.ts
+++ christmas-task/src/core/templates/settings.ts
@@ -2,7 +2,7 @@ class SettingsSections {
   static controls = `
 <div class="controls__filter-by-value">
   <h2 class="controls__filter-header">Sort by Value</h2>
-  <div id="filter-by-shape" class="filter__btns">
+  <div id="filter-by-shape" class="filter__btns filter__btns_shape">
     <h3 class="filter-btns__header">Shape</h3>
     <button class="btn" data-filter="ball"></button>
     <button class="btn" data-filter="bell"></button>
@@ -10,23 +10,23 @@ class SettingsSections {
     <button class="btn" data-filter="snowflake"></button>
     <button class="btn" data-filter="figurine"></button>
   </div>
-  <div id="filter-by-color" class="filter__btns">
+  <div id="filter-by-color" class="filter__btns filter__btns_color">
     <h3 class="filter-btns__header">Color</h3>
-    <button class="btn" data-filter="белый"></button>
-    <button class="btn" data-filter="желтый"></button>
-    <button class="btn" data-filter="красный"></button>
-    <button class="btn" data-filter="синий"></button>
-    <button class="btn" data-filter="зелёный"></button>
+    <button class="btn" data-filter="white"></button>
+    <button class="btn" data-filter="yellow"></button>
+    <button class="btn" data-filter="red"></button>
+    <button class="btn" data-filter="blue"></button>
+    <button class="btn" data-filter="green"></button>
   </div>
-  <div id="filter-by-size" class="filter__btns">
+  <div id="filter-by-size" class="filter__btns filter__btns_size">
     <h3 class="filter-btns__header">Size</h3>
-    <button class="btn" data-filter="большой"></button>
-    <button class="btn" data-filter="средний"></button>
-    <button class="btn" data-filter="малый"></button>
+    <button class="btn" data-filter="big"></button>
+    <button class="btn" data-filter="medium"></button>
+    <button class="btn" data-filter="small"></button>
   </div>
-  <div id="filter-by-favorite" class="filter__btns">
+  <div id="filter-by-favorite" class="filter__btns filter__btns_favourite">
     <h3 class="filter-btns__header">Favourite</h3>
-    <input type="checkbox" data-filter="favourite" />
+    <input type="checkbox" data-filter="favourite" class="input" />
   </div>
 </div>
 <div class="controls__filter-by-range">
